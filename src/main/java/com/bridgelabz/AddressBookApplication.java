3083b56f80c36e8be3c69d8115aedebd804d0169
sreeliptaa <sreelipta65@gmail.com>
2021-12-14 11:29:04 +0530

    Added : AddressBookApplication class after modified with documentation

--- src/main/java/com/bridgelabz/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/AddressBookApplication.java
@@ -3,6 +3,12 @@ package com.bridgelabz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Purpose : To simulate Address Book System
+ *
+ * @author : SREELIPTA
+ * @since : 14-12-2021
+ */
 @SpringBootApplication
 public class AddressBookApplication {
 
